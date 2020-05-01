e5e5bb337664f286c4bab5f10f76d2079744492b
Jim Safley <jimsafley@gmail.com>
2020-05-01 16:53:19 -0400

    Pull from tx

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,15 @@
 <?php
 return [
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => dirname(__DIR__) . '/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
     'service_manager' => [
         'factories' => [
             'ExtractText\ExtractorManager' => ExtractText\Service\Extractor\ManagerFactory::class,
